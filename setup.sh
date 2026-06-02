c0530d67aea79bb3ba42268046b0148d907dae8d
Shivansh Joshi <shivanshjoshi.5689@gmail.com>
2026-06-02 22:05:29 +0530

    Add mesa-zink and vulkan-loader-android to setup

--- setup.sh
+++ setup.sh
@@ -93,7 +93,7 @@ echo -e "${R} [${W}-${R}]${C} Checking for Installing packages...\n ${R}[${W}-${
 yes | pkg up
 pkg install root-repo x11-repo -y --no-install-recommends >/dev/null 2>&1
 pkg install tur-repo -y >/dev/null 2>&1
-pkg install proot-distro pulseaudio termux-x11-nightly virglrenderer-android mesa -y --no-install-recommends
+pkg install proot-distro pulseaudio termux-x11-nightly virglrenderer-android mesa mesa-zink virglrenderer-mesa-zink vulkan-loader-android -y --no-install-recommends
 }
 distro() {
    banner
